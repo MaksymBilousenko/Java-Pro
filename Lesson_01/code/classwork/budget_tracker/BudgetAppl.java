6ce04676cd60911dd62fd54f0cdc88efa8546fb7
“MaksymBilousenko” <maksimbelousenko11@gmail.com>
2024-06-19 23:29:27 +0200

    Lesson_02_budgetTracker

---
+++ Lesson_01/code/classwork/budget_tracker/BudgetAppl.java
@@ -0,0 +1,10 @@
+package classwork.budget_tracker;
+
+import classwork.budget_tracker.model.Menu;
+
+public class BudgetAppl {
+    public static void main(String[] args) {
+
+        Menu.printMenu();
+    }
+}
